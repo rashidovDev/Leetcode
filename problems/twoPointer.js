835a7c20d49d8cdb0791c62ef592e9cbdf04e61c
rashidovDev <anvarrashidov17@gmail.com>
2026-04-12 11:20:01 +0900

    Added moveZerosToFront

--- problems/twoPointer.js
+++ problems/twoPointer.js
@@ -46,7 +46,6 @@ function reverseArr2(arr){
 
 function isPalindrome(str){
     let cleanedStr = str.replace(/\s/g, '').toLowerCase()
-    console.log(cleanedStr)
     let left = 0
     let right = cleanedStr.length - 1
 
@@ -60,4 +59,70 @@ function isPalindrome(str){
     return true  
 }
 
+const arr = [0,1,0,4,3,12]
+function moveZeros(arr) {
+let index = 0
+
+for(let i=0; i < arr.length; i++){
+    if(arr[i] !== 0){
+        arr[index] = arr[i]
+        index++
+    }
+}
+
+while(index < arr.length){
+    arr[index] = 0
+    index++
+}
+
+return arr
+}
+
+function moveZeros2(arr){
+let left = 0
+for(let right=0; right<arr.length; right++){
+if(arr[right] !== 0){
+    [arr[left], arr[right]] = [arr[right], arr[left]]
+    left++
+}
+}
+return arr
+}
+
+
+function moveZeros3(arr){
+    let left = 0
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== 0){
+        arr[left] = arr[i]
+        left++
+        }
+    }
+
+    for(let i=left; left<arr.length; i++){
+        arr[i] = 0
+        left++
+    }
+
+    return arr
+}
+
+function moveZerosToFront(arr) {
+  let right = arr.length - 1;
+
+  for (let left = arr.length - 1; left >= 0; left--) {
+    if (arr[left] !== 0) {
+      arr[right] = arr[left];
+      right--;
+    }
+  }
+
+  for (let i = 0; i <= right; i++) {
+    arr[i] = 0;
+  }
+
+  return arr;
+}
+
+console.log(moveZerosToFront(arr))
 
